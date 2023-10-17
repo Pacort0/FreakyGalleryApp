021e66e3dde745806fa500675d6224ff4800180a
siu <francisco.rodriguez@iesnervion.es>
2023-10-17 23:31:38 +0200

    david suéltame el braso

--- app/src/main/java/com/example/freakygalleryapp/fotosAdapter.kt
+++ app/src/main/java/com/example/freakygalleryapp/fotosAdapter.kt
@@ -3,21 +3,21 @@ package com.example.freakygalleryapp
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.freakygalleryapp.databinding.ItemfotosBinding
 
-class fotosAdapter(private val fotos : List<Foto>):
+class fotosAdapter(private val fotos: List<Foto>) :
     RecyclerView.Adapter<fotosAdapter.ViewHolder>() {
-        class ViewHolder(val binding: ItemfotosBinding ):
-                RecyclerView.ViewHolder(binding.root){
-                    fun bind (foto: Foto){
-                        binding.descripcion.text=foto.descripcion
-                        Glide.with(binding.root.context)
-                            .load(foto.url)
-                            .into(binding.foto)
-                    }
-                }
+    class ViewHolder(val binding: ItemfotosBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(foto: Foto) {
+            Glide.with(binding.root.context)
+                .load(foto.url)
+                .into(binding.foto)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemfotosBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -29,6 +29,28 @@ class fotosAdapter(private val fotos : List<Foto>):
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var isImageFitToScreen = false
         holder.bind(fotos[position])
+        holder.binding.foto.setOnClickListener {
+            if (isImageFitToScreen) {
+                isImageFitToScreen = false
+                holder.binding.foto.layoutParams =
+                    LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                    )
+                holder.binding.linearL.setPadding(0,10,0,0)
+                holder.binding.foto.adjustViewBounds = false
+            } else {
+                isImageFitToScreen = true
+                holder.binding.foto.layoutParams =
+                    LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT
+                    )
+                holder.binding.linearL.setPadding(0,0,0,0)
+                holder.binding.foto.adjustViewBounds = true
+            }
+        }
     }
 }
